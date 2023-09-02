ad9d2802d1183da0e0e23e907e368e596616214f
Arrr Park <arrrpark@icloud.com>
2023-09-02 18:16:59 +0100

    add template for blog

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React from "react";
 import { Route, Link } from "react-router-dom";
-
 import "./App.css";
 import Page1 from "./pages/page1";
 import Page2 from "./pages/page2";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Menu = styled.div`
+  height: calc(100vh);
+  position: sticky;
+  align-content: center;
+
+  // top: 50px;
+  width: 250px; /* Adjust the width as needed */
+  background-color: #333; /* Sidebar background color */
+  color: #fff; /* Text color */
+  padding: 20px;
+`;
+
+const BigTitle = styled.div`
+  font-size: larger;
+`;
+
+const Right = styled.div`
+  flex: 5;
+  padding: 20px;
+  // background-color: #aaa;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
-      <Link to="/page1">page1</Link>
-      <Link to="/page2">page2</Link>
+    <Container>
+      <Menu>
+        <BigTitle>From Java to Kotlin</BigTitle>
 
-      <Route exact path="/page1">
-        <Page1 />
-      </Route>
-      <Route exact path="/page2">
-        <Page2 />
-      </Route>
-    </div>
+        <BigTitle>From Objective-C to Swift</BigTitle>
+      </Menu>
+      <Right>
+        <div>Hello</div>
+      </Right>
+    </Container>
   );
+}
+
+{
+  /* <Route exact path="/page1">
+          <Page1 />
+        </Route>
+        <Route exact path="/page2">
+          <Page2 />
+        </Route> */
 }
 
 export default App;
